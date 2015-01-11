8c914fe8e362991f5b33e1f10cc46160c7c6ffbf
Neta Ravid <neta16@meet.mit.edu>
2015-01-11 19:19:30 +0200

    added functions

--- OOP/oop.py
+++ OOP/oop.py
@@ -6,13 +6,36 @@ class Animal:
 		self.size=size
 
 	def print_all(self):
-		print (self.name)
-		print (self.age)
-		print (self.color)
-		print (self.size)
+		print ("Animal's name is: " + self.name)
+		print ("Animal's age is: " + str(self.age))
+		print ("Animal's color is: " + self.color)
+		print ("Animal's size is: " + self.size)
 	
-elephant = Animal (name="augustus",age=157 ,color="blue",size="XXL") 
-elephant.print_all()
+	def eating(self,food):
+		print (self.name, "is eating",food)
+		
+	
+	def sleeping(self):
+		print (self.name, "is sleeping")
 
-penguin = Animal (name="gloria",age=32 ,color="rainbow",size="S") 
+	def bathroom(self):
+		print (self.name, "went to the bathroom")
+
+print (" ")
+print ("The Elephant: ")	
+elephant = Animal (name="Augustus",age=157 ,color="blue",size="XXL") 
+elephant.print_all()
+elephant.eating(food="meat")
+elephant.sleeping()
+elephant.bathroom()
+
+print (" ")
+print ("The Penguin: ")
+penguin = Animal (name="Gloria",age=32 ,color="rainbow",size="S") 
 penguin.print_all()
+penguin.eating(food="fish")
+penguin.sleeping()
+penguin.bathroom() 
+
+print (" ")
+
